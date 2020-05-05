4e5b8d9463a4842214d5c89cfb8bc8221118428f
Dr Isme <drisme777@gmail.com>
2020-05-05 17:06:45 +0200

    slight layout update with LayoutBuilder

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,22 +1,23 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "ca042fbabb4e49db8de6ba36473c4e6d",
+  "assets/AssetManifest.json": "c622d1d4da8a4b0f028c0890e4e220f4",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "6d613339b9503442c01a93585c446afd",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/quiz_data/categories.json": "d450f491c0e2b27eba01dd9d63557314",
-"assets/quiz_data/categories_old.json": "2d7821c530077328d71dcd5976ff7d06",
-"assets/quiz_data/cephalosporine.json": "b15c6833be1f1e4dce3b201189b2875f",
-"assets/quiz_data/kokken.json": "70ee940e7423ff6351543f101d352ebf",
+"assets/quiz_data/categories_old.json": "0065102f200f5931478bbe755fdac4c0",
+"assets/quiz_data/conditionals.json": "5160d8625e9abb38b0cb38e3cac6648e",
+"assets/quiz_data/ing_forms.json": "21bd8b1f4e8b000dc728ad8c2a5925fc",
+"assets/quiz_data/modals.json": "2465717ef81789fac920118a01c81689",
+"assets/quiz_data/quantifiers.json": "36373e3931f6fe45ff16c8a35ccf3836",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a56465d94e800efb6a672c7479998c25",
-"/": "a56465d94e800efb6a672c7479998c25",
-"main.dart.js": "403386f7cf6acb3abac685fdc51501f5",
-"manifest.json": "62153e63fe75294649a52f327e101bbe"
+"index.html": "9fa9935adf1728106a9d94b8d5311c82",
+"/": "9fa9935adf1728106a9d94b8d5311c82",
+"main.dart.js": "aa7ec580ab9768ef6cfb29cd09eb2ba8",
+"manifest.json": "2b42b44cf86e0f6fe06d5006e7de8970"
 };
 
 self.addEventListener('activate', function (event) {
